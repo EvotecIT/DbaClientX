02164130f572b6e2ae4cb12d9f041ad39ce75359
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-13 21:02:16 +0200

    Add PowerShell cmdlets for invoking and creating SQL queries

---
+++ DbaClientX.PowerShell/CmdletIInvokeDbaXQuery.cs
@@ -0,0 +1,90 @@
+﻿namespace DBAClientX.PowerShell;
+
+[Cmdlet(VerbsLifecycle.Invoke, "DbaXQuery", DefaultParameterSetName = "Compatibility", SupportsShouldProcess = true)]
+[CmdletBinding()]
+public sealed class CmdletIInvokeDbaXQuery : PSCmdlet {
+    [Parameter(Mandatory = true, ParameterSetName = "DefaultCredentials")]
+    [Alias("DBServer", "SqlInstance", "Instance")]
+    public string Server { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = "DefaultCredentials")]
+    public string Database { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = "DefaultCredentials")]
+    public string Query { get; set; }
+
+    [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
+    public int QueryTimeout { get; set; }
+
+    [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
+    [Alias("As")]
+    public ReturnType ReturnType { get; set; } = ReturnType.DataRow;
+
+    private ActionPreference ErrorAction;
+
+    /// <summary>
+    /// Begin processing method for PowerShell cmdlet
+    /// </summary>
+    protected override void BeginProcessing() {
+        // Get the error action preference as user requested
+        // It first sets the error action to the default error action preference
+        // If the user has specified the error action, it will set the error action to the user specified error action
+        ErrorAction = (ActionPreference)this.SessionState.PSVariable.GetValue("ErrorActionPreference");
+        if (this.MyInvocation.BoundParameters.ContainsKey("ErrorAction")) {
+            string errorActionString = this.MyInvocation.BoundParameters["ErrorAction"].ToString();
+            if (Enum.TryParse(errorActionString, true, out ActionPreference actionPreference)) {
+                ErrorAction = actionPreference;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Process method for PowerShell cmdlet
+    /// </summary>
+    protected override void ProcessRecord() {
+        var sqlServer = new DBAClientX.SqlServer {
+            ReturnType = ReturnType,
+            CommandTimeout = QueryTimeout
+        };
+        try {
+            var result = sqlServer.SqlQuery(Server, Database, true, Query);
+            if (result != null) {
+                if (ReturnType == ReturnType.PSObject) {
+                    //var resultConverted = result as DataTable;
+                    foreach (DataRow row in ((DataTable)result).Rows) {
+                        WriteObject(DataRowToPSObject(row));
+                    }
+                } else {
+                    WriteObject(result, true);
+                }
+            }
+        } catch (Exception ex) {
+            WriteWarning($"Invoke-DbaXQuery - Error querying SqlServer: {ex.Message}");
+            if (ErrorAction == ActionPreference.Stop) {
+                throw;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Convert DataRow to PSObject
+    /// </summary>
+    /// <param name="row"></param>
+    /// <returns></returns>
+    private static PSObject DataRowToPSObject(DataRow row) {
+        PSObject psObject = new PSObject();
+
+        if (row != null && (row.RowState & DataRowState.Detached) != DataRowState.Detached) {
+            foreach (DataColumn column in row.Table.Columns) {
+                Object value = null;
+                if (!row.IsNull(column)) {
+                    value = row[column];
+                }
+
+                psObject.Properties.Add(new PSNoteProperty(column.ColumnName, value));
+            }
+        }
+
+        return psObject;
+    }
+}
---
+++ DbaClientX.PowerShell/CmdletNewDbaXQuery.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+
+namespace DBAClientX.PowerShell;
+
+[Cmdlet(VerbsCommon.New, "DbaXQuery", DefaultParameterSetName = "Compatibility", SupportsShouldProcess = true)]
+[CmdletBinding()]
+public sealed class CmdletNewDbaXQuery : PSCmdlet {
+    [Parameter(Mandatory = true, Position = 0)]
+    public string TableName { get; set; }
+
+    [Parameter(Mandatory = false)]
+    public SwitchParameter Compile { get; set; }
+
+    [Parameter(Mandatory = false)]
+    public SwitchParameter DontUseLegacyPagination { get; set; }
+
+    private ActionPreference errorAction;
+
+    protected override void BeginProcessing() {
+        // Get the error action preference as user requested
+        // It first sets the error action to the default error action preference
+        // If the user has specified the error action, it will set the error action to the user specified error action
+        errorAction = (ActionPreference)this.SessionState.PSVariable.GetValue("ErrorActionPreference");
+        if (this.MyInvocation.BoundParameters.ContainsKey("ErrorAction")) {
+            string errorActionString = this.MyInvocation.BoundParameters["ErrorAction"].ToString();
+            if (Enum.TryParse(errorActionString, true, out ActionPreference actionPreference)) {
+                errorAction = actionPreference;
+            }
+        }
+    }
+
+    protected override void ProcessRecord() {
+        var compiler = new SqlKata.Compilers.SqlServerCompiler() {
+            UseLegacyPagination = !DontUseLegacyPagination
+        };
+        var query = new SqlKata.Query(TableName);
+        if (!Compile) {
+            WriteObject(query);
+        } else {
+            WriteObject(compiler.Compile(query).ToString());
+        }
+    }
+}
