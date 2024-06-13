eae91fe9507bc5ea745b6610d6699c839ec9b04f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-13 21:02:11 +0200

    chore: Add DbaClientX project files and classes

---
+++ DbaClientX/ReturnType.cs
@@ -0,0 +1,14 @@
+﻿namespace DBAClientX;
+
+/// <summary>
+/// Return type for SQL query
+/// </summary>
+public enum ReturnType {
+    DataSet,
+    DataTable,
+    DataRow,
+    /// <summary>
+    /// Works only with PowerShell
+    /// </summary>
+    PSObject
+}
---
+++ DbaClientX/SqlServer.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DBAClientX;
+
+/// <summary>
+/// This class is used to connect to SQL Server
+/// </summary>
+public class SqlServer {
+    public ReturnType ReturnType;
+    public int CommandTimeout { get; set; }
+
+    public object SqlQuery(string serverOrInstance, string database, bool integratedSecurity, string query) {
+        var connectionString = new SqlConnectionStringBuilder {
+            DataSource = serverOrInstance,
+            InitialCatalog = database,
+            IntegratedSecurity = integratedSecurity,
+        }.ConnectionString;
+
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        var command = new SqlCommand(query, connection);
+        if (command.CommandTimeout > 0) {
+            command.CommandTimeout = CommandTimeout;
+        }
+        var dataAdapter = new SqlDataAdapter(command);
+        var dataSet = new System.Data.DataSet();
+
+        dataAdapter.Fill(dataSet);
+
+        if (ReturnType == ReturnType.DataRow || ReturnType == ReturnType.PSObject) {
+            if (dataSet.Tables.Count > 0) {
+                return dataSet.Tables[0];
+            }
+        }
+
+        if (ReturnType == ReturnType.DataSet) {
+            return dataSet;
+        }
+
+        if (ReturnType == ReturnType.DataTable) {
+            return dataSet.Tables;
+        }
+
+        return null;
+    }
+}
