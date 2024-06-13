6578e2423fd8fce55239e3647077be13b40cec55
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-13 21:02:36 +0200

    feat: Add example script for querying SQL Server
    The commit adds a new PowerShell script `Example.QuerySqlServer.ps1` that demonstrates how to use the `Invoke-DbaXQuery` cmdlet from the `DBAClientX` module to query SQL Server databases. The script includes multiple examples of querying different tables and returning the results in various formats.
    
    This commit addresses the need for providing users with a practical usage example of the `Invoke-DbaXQuery` cmdlet and helps them understand how to interact with SQL Server databases using the `DBAClientX` module.

---
+++ Module/Examples/Example.QuerySqlServer.ps1
@@ -0,0 +1,17 @@
+﻿Clear-Host
+Import-Module $PSScriptRoot\..\DBAClientX.psd1 -Force -Verbose
+
+$T = Invoke-DbaXQuery -Query "SELECT * FROM sys.databases" -Server "SQL1" -Database "master"
+$T | Format-Table
+
+$T1 = Invoke-DbaXQuery -Query "SELECT * FROM MSreplication_options" -Server "SQL1" -Database "master" -ReturnType DataRow
+$T1 | Format-Table
+
+$T2 = Invoke-DbaXQuery -Query "SELECT * FROM MSreplication_options" -Server "SQL1" -Database "master" -ReturnType PSObject
+$T2 | Format-Table
+
+$T3 = Invoke-DbaXQuery -Query "SELECT * FROM MSreplication_options" -Server "SQL1" -Database "master" -ReturnType DataSet
+$T3 | Format-Table
+
+$T4 = Invoke-DbaXQuery -Query "SELECT * FROM MSreplication_options" -Server "SQL1" -Database "master" -ReturnType DataTable
+$T4 | Format-Table
